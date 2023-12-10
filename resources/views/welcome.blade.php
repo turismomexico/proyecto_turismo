909c30e0fe35fd535a37699b26fe7cd3c313eb7e
Carlos_V1 <carlosdanielepimenio@gmail.com>
2023-12-09 19:05:13 -0600

    Prueba&Error

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+      <a class="nav-link" href="datos">Diccionario de Datos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="usos">Casos de Uso</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="diagrama">Diagrama de secuencias</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="algoritmos">Algoritmos Genéricos</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    table {
+      border-collapse: collapse;
+      width: 100%;
+    }
+
+    th, td {
+      border: 1px solid #ddd;
+      padding: 8px;
+      text-align: left;
+    }
+
+    th {
+      background-color: #f2f2f2;
+    }
+  </style>
+  <title>Casos de Uso: Sistema de Gestión para un Cinepolis</title>
+</head>
+<body>
+
+<div>
+    <h4 class="mb-4">Casos de Uso: Sistema de Gestión para un Cinepolis</h4>
+
+
+<table>
+  <thead>
+    <tr>
+      <th>Sección</th>
+      <th>Contenido</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Casos de Uso</td>
+      <td>Sistema de Gestión para un Cinepolis</td>
+    </tr>
+    <tr>
+      <td>Ejemplo</td>
+      <td>Un sistema de gestión de ventas para un cinepolis que nos permita registrar y realizar un seguimiento de los clientes, productos, proveedores y otras entidades relacionadas.</td>
+    </tr>
+    <tr>
+      <td>Escenario de Uso</td>
+      <td>Un trabajador necesita comprender la relación entre las entidades "VENTAS" y "PRODUCTOS" para realizar una optimización en la búsqueda de productos vendidos en un período de tiempo específico. Utiliza el diccionario de datos para entender la estructura de la base de datos.</td>
+    </tr>
+    <tr>
+      <td>Beneficios</td>
+      <td>
+        <ul>
+          <li>Proporciona una guía clara sobre la estructura de la base de datos.</li>
+          <li>Facilita la comprensión de las relaciones entre las entidades.</li>
+          <li>Ayuda a los desarrolladores a escribir consultas SQL eficientes y precisas.</li>
+          <li>Permite a los desarrolladores y analistas entender la semántica de los datos almacenados.</li>
+        </ul>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+</body>
+</html>
